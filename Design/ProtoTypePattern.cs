f8cfb6a9dd0ebc6db4ddc4b6d690569b5c167d2e
lifejoyforpy <864292757@qq.com>
2019-11-19 19:48:57 +0800

    protoType design pattern

--- Design/ProtoTypePattern.cs
+++ Design/ProtoTypePattern.cs
@@ -1,18 +1,22 @@
 using System;
+using System.Collections.Generic;
+
 namespace DesignPattern.Design
 {
     public class ProtoTypePattern
     {
         
     }
+
     public class Person:ICloneable
-    {
-        
-        public object Clone(){;
-              Person person=null;
-              person.Clone();
-              return person;
+    {        
+           public object Clone(){;
+             return  this.MemberwiseClone();
         }
     }
+
+   
+
+
 
 }
